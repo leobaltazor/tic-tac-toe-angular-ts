e581f7173580ed6dbd9c90967dd9aaf6bab89567
oleksii.chupruna <alexsei@boundstart.com>
2018-11-15 18:44:07 +0200

    end day

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -42,6 +42,36 @@ export class TableComponent implements OnInit {
     const {c, r} = e.target.dataset;
     this.table[r][c] = this.player;
     // e.target.innerHTML = this.player;
+    this.whoseTurn();
+  }
+  whoseTurn(): void {
     this.player = this.player === 'X' ? 'O' : 'X';
+  }
+  whoWin() {
+    this.table.forEach(e => {
+      let str = e.join('');
+      this.gameOwer(str);
+    });
+    for (var i = 0; i < 3; i++) {
+      var str = `${arr[0][i]}${arr[1][i]}${arr[2][i]}`;
+      gameOwer(str);
+    }
+    var str = `${arr[0][0]}${arr[1][1]}${arr[2][2]}`;
+    gameOwer(str);
+    var str = `${arr[2][0]}${arr[1][1]}${arr[0][2]}`;
+    gameOwer(str);
+  }
+  gameOwer(str) {
+    setTimeout(() => {
+      if (str === "XXX") {
+        stat(1, 0);
+        confirmm(str[0]);
+        _whoseTurn.innerHTML = "Победил игрок X";
+      } else if (str === "OOO") {
+        confirmm(str[0]);
+        stat(0, 1);
+        _whoseTurn.innerHTML = "Победил игрок О";
+      }
+    }, 100);
   }
 }
